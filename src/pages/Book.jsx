3e5057ec57ff8bd19e5f30bd76a2c6dc3e88c31e
trekvartista <isken_150502@mail.ru>
2022-05-22 04:18:37 +0600

    todo: admin page, book page
    Need to combine this logic in redux store

---
+++ src/pages/Book.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const Book = () => {
+
+    const { id } = useParams();
+
+    return (
+        <div>
+            Book Number {id}
+        </div>
+    );
+};
+
+export default Book;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
+import Book from "./pages/Book";
 import Library from "./pages/Library";
-import { LIBRARY_ROUTE } from "./utils/consts";
+import { BOOK_ROUTE, LIBRARY_ROUTE } from "./utils/consts";
 
 export const publicRoutes = [
     {
         path: LIBRARY_ROUTE,
         Component: Library
+    },
+    {
+        path: BOOK_ROUTE + '/:id',
+        Component: Book
     }
 ]
