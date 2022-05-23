3ee7bae122447726aa50ae334bcb7576809e5246
trekvartista <isken_150502@mail.ru>
2022-05-23 23:43:37 +0600

    Сhanged postman API URL

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -6,4 +6,5 @@ export const BOOKS_ROUTE = "/books";
 export const BOOK_ROUTE = "/book";
 // export const WISH_LIST_ROUTE = '/login'
 export const API_URL_ROUTE =
-    "https://03ebc1c4-6445-4771-9cd7-b79dddec2006.mock.pstmn.io/";
+    "https://f1316cfb-0e42-4050-bc51-72b2d1a967ae.mock.pstmn.io"
+    // "https://03ebc1c4-6445-4771-9cd7-b79dddec2006.mock.pstmn.io/";
